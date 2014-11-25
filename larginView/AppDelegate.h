8e1c7fd838ea37c4b0dc06e7ff0d7b06c80d0133
Robb Schiller <robbschiller@gmail.com>
2014-11-25 07:14:53 -0500

    Initial Commit

---
+++ larginView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  larginView
+//
+//  Created by Robb Schiller on 11/25/14.
+//  Copyright (c) 2014 Robb Schiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
