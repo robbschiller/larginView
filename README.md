30ad332a10f83f4bc9063203fad13d16f3d2acfb
Robb Schiller <robbschiller@gmail.com>
2014-11-30 07:33:20 -0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Learning  
+- Keyboard control
+  - "next button"
+  - resigning First Responder
+  - sliding content with keyboard
+- Autolayout
+  - programmatic syntax
+  - "nearest" positioning
--- larginView/ViewController.m
+++ larginView/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+// Class extension
 @interface ViewController ()
+
+@property (nonatomic, strong) UITextField *usernameTextField;
+@property (nonatomic, strong) UITextField *passwordTextField;
+@property (nonatomic, strong) UIButton *loginButton;
 
 @end
 
@@ -16,12 +21,80 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	[self setupViews];
+	[self setupContraints];
+	
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+- (void)setupViews {
+
+	self.view.backgroundColor = [UIColor whiteColor];
+	
+	// Username
+	self.usernameTextField = [[UITextField alloc] init];
+	self.usernameTextField.translatesAutoresizingMaskIntoConstraints = NO;
+	self.usernameTextField.borderStyle = UITextBorderStyleNone;
+	self.usernameTextField.backgroundColor = [UIColor grayColor];
+	self.usernameTextField.placeholder = @"username";
+	
+	[self.view addSubview:self.usernameTextField];
+	
+	// Password
+	self.passwordTextField = [[UITextField alloc] init];
+	self.passwordTextField.translatesAutoresizingMaskIntoConstraints = NO;
+	self.passwordTextField.borderStyle = UITextBorderStyleNone;
+	self.passwordTextField.backgroundColor = [UIColor grayColor];
+	self.passwordTextField.secureTextEntry = YES;
+	self.passwordTextField.placeholder = @"password";
+	
+	[self.view addSubview:self.passwordTextField];
+	
+	// Button
+	self.loginButton = [[UIButton alloc] init];
+	self.loginButton.translatesAutoresizingMaskIntoConstraints = NO;
+	[self.loginButton setTitle:@"Login" forState:UIControlStateNormal];
+	self.loginButton.backgroundColor = [UIColor grayColor];
+	
+	[self.view addSubview:self.loginButton];
+	
+}
+
+- (void)setupContraints {
+	
+	NSString *visualFormat = @"|-20-[_usernameTextField(==_passwordTextField)]-20-|";
+	NSDictionary *views = NSDictionaryOfVariableBindings(_usernameTextField, _passwordTextField, _loginButton);
+	NSArray *contraints = [NSLayoutConstraint constraintsWithVisualFormat:visualFormat
+																																 options:0
+																																 metrics:nil
+																																 views:views];
+	
+	[self.view addConstraints:contraints];
+
+	visualFormat = @"|-20-[_passwordTextField]-20-|";
+	contraints = [NSLayoutConstraint constraintsWithVisualFormat:visualFormat
+																										options:0
+																										metrics:nil
+																										views:views];
+	
+	[self.view addConstraints:contraints];
+	
+	visualFormat = @"|-80-[_loginButton]-80-|";
+	contraints = [NSLayoutConstraint constraintsWithVisualFormat:visualFormat
+																										options:0
+																										metrics:nil
+																										views:views];
+	
+	[self.view addConstraints:contraints];
+	
+	visualFormat = @"V:|-100-[_usernameTextField(==44)]-20-[_passwordTextField(==_usernameTextField)]-40-[_loginButton(==44)]";
+	contraints = [NSLayoutConstraint constraintsWithVisualFormat:visualFormat
+																										options:0
+																										metrics:nil
+																										views:views];
+	
+	[self.view addConstraints:contraints];
+	
 }
 
 @end
